2808f6d8ca06043de66df1c60f7cce8f030f2ab5
Carlos André <andre.tjlba@gmail.com>
2022-05-07 13:25:23 -0300

    Estruturas de decisão

---
+++ src/academy/devdojo/maratonajava/introducao/SwitchCase.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.introducao;
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Informe o dia da semana de 1 a 7: ");
+        byte diaSemana = scan.nextByte();
+
+        switch(diaSemana) {
+            case 1:
+                System.out.println("1 - Domingo");
+                break;
+            case 2:
+                System.out.println("2 - Segunda");
+                break;
+            case 3:
+                System.out.println("3 - Terça");
+                break;
+            case 4:
+                System.out.println("4 - Quarta");
+                break;
+            case 5:
+                System.out.println("5 - Quinta");
+                break;
+            case 6:
+                System.out.println("6 - Sexta");
+                break;
+            case 7:
+                System.out.println("7 - Sábado");
+                break;
+            default:
+                System.out.println("Não é um dia da semana");
+        }
+
+    }
+}
